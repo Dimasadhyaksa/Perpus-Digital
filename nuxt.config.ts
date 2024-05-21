9a941cc147e619fb802c94aa952912bbbbbd36c5
Dimasadhyaksa <143145756+Dimasadhyaksa@users.noreply.github.com>
2024-05-21 07:43:05 +0700

    Add files via upload

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+
+  modules: ["@nuxtjs/supabase"],
+  supabase: {redirect: false},
+
   devtools: { enabled: false },
   css: ["bootstrap/dist/css/bootstrap.min.css"],
 })
